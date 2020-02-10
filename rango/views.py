d88aafc35ef02e3f68c88f388ffecbc1613f4cfa
Mairi Sillars <2404079s@student.gla.ac.uk>
2020-02-10 18:19:57 +0000

    Was finally able to link from about.html to index

--- rango/views.py
+++ rango/views.py
@@ -6,11 +6,13 @@ def index(request):
 	# Construct a dictionary to pass to the template engine as its context.
 	# Note the key boldmessage matches to {{ boldmessage }} in the template
 	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-	
 	# Return a rendered response to send to the client.
 	#We make use of the shortcut function to make our lives easier.
 	# Note that the first parameter is the template we wish to use
 	return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-	return HttpResponse("<a href='/rango/'>Index</a> Rango says here is the about page.")
+	about_context_dict = {'boldmessage': 'This tutorial has been put together by Mairi Sillars'}
+	return render(request, 'rango/about.html', context=about_context_dict)
+	# can remove following line once the tests for CHAPTER 4 passed
+	#return HttpResponse("<a href='/rango/'>Index</a> Rango says here is the about page.")
