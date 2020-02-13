bd645533bf4e8c25fd297169e5e853b7592d4f45
Mairi Sillars <2404079s@student.gla.ac.uk>
2020-02-13 11:20:29 +0000

    Passed tests for chapter 7

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,11 @@ from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
 
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+from django.shortcuts import redirect
+from django.urls import reverse
+
 def index(request):
 	# Query the db for a list of all categories currently stored.
 	# Order the categories by the number of likes in descending order.
@@ -46,3 +51,50 @@ def show_category(request, category_name_slug):
 	
 	return render(request, 'rango/category.html', context=context_dict)
 	
+# Display the category form and handle the posting of form data
+def add_category(request):
+	form = CategoryForm()
+	
+	if request.method == 'POST':
+		form = CategoryForm(request.POST)
+		
+		if form.is_valid():
+			# Save the new category to the db
+			form.save(commit=True)
+			return redirect('/rango/')
+		else:
+			# The form contained errors
+			print(form.errors)
+			
+	return render(request, 'rango/add_category.html', {'form': form})
+	
+def add_page(request, category_name_slug):
+	try:
+		category = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		category = None
+
+	# You cannot add a page to a Category that does not exist
+	if category is None:
+		return redirect('/rango/')
+
+	form = PageForm()
+	
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		
+		if form.is_valid():
+			if category:
+				# Save the new page to the db
+				page = form.save(commit=False)
+				page.category = category
+				page.views = 0
+				page.save()
+				
+				return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
+		else:
+			# The form contained errors
+			print(form.errors)
+			
+	context_dict = {'form': form, 'category': category}		
+	return render(request, 'rango/add_page.html', context=context_dict)
